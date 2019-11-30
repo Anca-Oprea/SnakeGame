2a0439c55fb7fbe49224fc6e2d69f2fc70153c45
Oprea Anca <opreaanca1987@gmail.com>
2019-11-30 21:40:12 +0100

    Updates

---
+++ precache-manifest.42b0bed4aa05790cf1734dc63b6bfc0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984756b38da58dfe21fbe5b3292320af",
+    "url": "/Anca-Oprea/SnakeGame.git/index.html"
+  },
+  {
+    "revision": "9b5e9bdc0ffd440d79b1",
+    "url": "/Anca-Oprea/SnakeGame.git/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b5c058d239b753f4d819",
+    "url": "/Anca-Oprea/SnakeGame.git/static/js/2.1bb91026.chunk.js"
+  },
+  {
+    "revision": "9b5e9bdc0ffd440d79b1",
+    "url": "/Anca-Oprea/SnakeGame.git/static/js/main.d7ff6919.chunk.js"
+  },
+  {
+    "revision": "c574de73eea42089c3f2",
+    "url": "/Anca-Oprea/SnakeGame.git/static/js/runtime-main.63194ed7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Anca-Oprea/SnakeGame.git/static/media/logo.25bf045c.svg"
+  }
+]);
